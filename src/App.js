4ff5ba709b9764f53035cb57314018ab0ff7db92
Ngan LE <ngan.ltkn2911@gmail.com>
2023-11-29 14:55:13 +0100

    re-code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import ShoesShop from './ShoesShop_Redux/ShoesShop';
 
 function App() {
   return (
     <div className="App">
-      <ShoesShop />
+      
     </div>
   );
 }
--- src/ShoesShop_Redux/ShoesShop.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class ShoesShop extends Component {
-  render() {
-    return (
-      <div>ShoesShop</div>
-    )
-  }
-}
